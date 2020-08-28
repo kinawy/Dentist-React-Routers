9c5e3826f60eedb10e27bf51a353dbd0ccf6dc4e
kinawy <sameh.yasser.kinawy@gmail.com>
2020-08-28 14:30:04 -0400

    Adds procdures list to procedures page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Home from "./Home";
 import Procedures from "./Procedures";
-import Contact from './Contact'
+import Contact from "./Contact";
 
 function App() {
+  const procedures = [
+    "Deep Cleaning",
+    "Oral Surgery",
+    "Laughing Gas",
+    "Root Canal",
+    "Crowns",
+    "Fillings",
+  ];
   return (
     <Router>
-    <div className="App">
-      <nav>
-        <Link to="/">Home?</Link>{' '}
-        <Link to="/procedures">See Our Procedures</Link>{' '}
-        <Link to="/contact">Contact Us</Link>{' '}
-      </nav>
-      <Route exact path="/" component={Home} />
-      <Route path='/procedures' component={Procedures}/>
-      <Route path="/contact" component={Contact} />
-    </div>
+      <div className="App">
+        <nav>
+          <Link to="/">Home?</Link>{" "}
+          <Link to="/procedures">See Our Procedures</Link>{" "}
+          <Link to="/contact">Contact Us</Link>{" "}
+        </nav>
+        <Route exact path="/" component={Home} />
+        <Route path="/procedures" render={() => <Procedures procedures={procedures}/>}/>
+        <Route path="/contact" component={Contact} />
+      </div>
     </Router>
   );
 }
--- src/Procedures.js
+++ src/Procedures.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const Procedures = () => {
+const Procedures = (props) => {
+    let listPros = props.procedures.map((pros, i) => {
+       return <p key={i}>{pros}</p>
+    })
 
     return(
         <div>
             <h1>Procedures</h1>
+            {listPros}
         </div>
     )
 }
